9c523f357cde0ca8f095c9d044e028ec7c4ea57a
Hien Khieu <hienkhieu@yahoo.com>
2016-07-17 22:33:04 -0400

    failed test

--- Tests/Data/EF/InstructorCollectionShould.cs
+++ Tests/Data/EF/InstructorCollectionShould.cs
@@ -1,4 +1,4 @@
-using aspnet_core;
+using aspnet_core.Data.Ef;
 using aspnet_core.models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -7,25 +7,28 @@ using Moq;
 namespace aspnet_core.tests
 {
     /// <summary>
-    /// InstructorCollectionShould test class 
+    /// InstructorCollectionShould
     /// </summary>
     [TestClass]
     public class InstructorCollectionShould
     {
-        
-
         /// <summary>
-        /// Add item
+        /// Add Item
         /// </summary>
         [TestMethod]
         public void AddItem()
         {
             var mockSet = new Mock<DbSet<Instructor>>();
 
+            var mocInstructor = new Mock<Instructor>();
+
             var mockContext = new Mock<DataContext>();
             mockContext.Setup(m => m.Instructors).Returns(mockSet.Object);
 
-            
+            var mockInstructorCollection = new InstructorCollection(mockContext.Object);
+            var mockUnitOfWork = new UnitOfWork(mockContext.Object);
+
+            mockInstructorCollection.Add(mocInstructor.Object).ConfigureAwait(false);
 
             mockSet.Verify(m => m.Add(It.IsAny<Instructor>()), Times.Once());
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
